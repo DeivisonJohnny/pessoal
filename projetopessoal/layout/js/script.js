773297adc59fa94771e3ab14079e029c681652d8
DeivisonJohnny <deivisonjohnny@gmail.com>
2022-07-03 09:27:48 -0300

    Atualização script

---
+++ projetopessoal/layout/js/script.js
@@ -0,0 +1,10 @@
+var btt = document.querySelector('#botao')
+var sp = document.getElementsByTagName('span') [0]
+
+
+function clicou() {
+    btt.style.background = 'red'
+    sp.innerHTML = 'Botão ilustrativo'
+}
+
+// Botão vai ficar vermelho e mostrar msg botão ilustrativo
